b578e0ecc22036d2a31a23fe8fa4122bc155a2c2
Dima <dimaka888@yahoo.com>
2017-05-05 15:11:50 +0000

    initial

---
+++ form.js
@@ -0,0 +1,18 @@
+document.querySelector("input[type=submit]").addEventListener("click", sendForm);
+
+function sendForm(e) {
+    var form = document.querySelector("form");
+    if (form.checkValidity()) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/dimaka888@yahoo.com",
+            method: "POST",
+            data: {
+                clientName: $("#text").val(),
+                clientEmail: $("#email").val(),
+                comment: $("#message").val()
+            },
+            dataType: "json"
+        });
+    }
+}
